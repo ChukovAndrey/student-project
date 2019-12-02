83e7ffeb6408680c7a051b8ab1b366d26b67e73c
ChukovAndrey <chukov.info@gmail.com>
2019-12-03 00:10:24 +0300

    Added basic classes for the project

---
+++ src/SaveStudentOrder.java
@@ -0,0 +1,23 @@
+public class SaveStudentOrder
+{
+
+    public static void main(String[] args) {
+
+        StudentOrder so;
+        so = new StudentOrder();
+        so.nFirstName = "Алексей";
+        so.hLastName = "Петров";
+        so.wFirstName = "Галина";
+        so.wLastName = "Петрова";
+
+        long ans = saveStudentOrder(so);
+        System.out.println(ans);
+    }
+
+    static long saveStudentOrder(StudentOrder studentOrder) {
+        long answer = 199;
+        System.out.println("saveStudentOrder" + studentOrder.hLastName);
+
+        return answer;
+    }
+}
---
+++ src/StudentOrder.java
@@ -0,0 +1,8 @@
+public class StudentOrder
+{
+    String nFirstName;
+    String hLastName;
+    String wFirstName;
+    String wLastName;
+
+}
