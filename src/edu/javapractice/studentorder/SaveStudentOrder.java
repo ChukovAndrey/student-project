74c815b9b8c7453db56fb309c379e810641323ff
ChukovAndrey <chukov.info@gmail.com>
2019-12-09 22:24:00 +0300

    Added getters and setters for StudentOrder class.

--- src/edu/javapractice/studentorder/SaveStudentOrder.java
+++ src/edu/javapractice/studentorder/SaveStudentOrder.java
@@ -9,10 +9,10 @@ public class SaveStudentOrder
 
         StudentOrder so;
         so = new StudentOrder();
-        so.nFirstName = "Алексей";
-        so.hLastName = "Петров";
-        so.wFirstName = "Галина";
-        so.wLastName = "Петрова";
+        so.sethFirstName("Алексей");
+        so.sethLastName("Петров");
+        so.setwFirstName("Галина");
+        so.setwLastName("Петрова");
 
         long ans = saveStudentOrder(so);
         System.out.println(ans);
@@ -20,7 +20,7 @@ public class SaveStudentOrder
 
     static long saveStudentOrder(StudentOrder studentOrder) {
         long answer = 199;
-        System.out.println("saveStudentOrder" + studentOrder.hLastName);
+        System.out.println("saveStudentOrder" + studentOrder.gethLastName());
 
         return answer;
     }
